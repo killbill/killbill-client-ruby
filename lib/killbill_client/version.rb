c48c76098f086404dfc01a28b61067c20ea9a3aa
Pierre-Alexandre Meyer <pierre@mouraf.org>
2013-10-22 14:51:35 -0400

    version: bump to 0.5.0
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/killbill_client/version.rb
+++ lib/killbill_client/version.rb
@@ -1,8 +1,8 @@
 module KillBillClient
   module Version
     MAJOR = 0
-    MINOR = 4
-    PATCH = 6
+    MINOR = 5
+    PATCH = 0
     PRE = nil
 
     VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.').freeze
