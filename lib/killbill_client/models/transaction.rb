f58471772bb5e55e9f4b6df6c8c5603fbfc05909
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-07-21 10:02:52 -0700

    models: handle nil query parameters gracefully for transactions
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/killbill_client/models/transaction.rb
+++ lib/killbill_client/models/transaction.rb
@@ -5,12 +5,13 @@ module KillBillClient
       has_many :audit_logs, KillBillClient::Model::AuditLog
 
       def auth(account_id, payment_method_id = nil, user = nil, reason = nil, comment = nil, options = {})
+        query_map                   = {}
+        query_map[:paymentMethodId] = payment_method_id unless payment_method_id.nil?
+
         @transaction_type   = 'AUTHORIZE'
         created_transaction = self.class.post "#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/#{account_id}/payments",
                                               to_json,
-                                              {
-                                                  :paymentMethodId => payment_method_id
-                                              },
+                                              query_map,
                                               {
                                                   :user    => user,
                                                   :reason  => reason,
@@ -20,12 +21,13 @@ module KillBillClient
       end
 
       def purchase(account_id, payment_method_id = nil, user = nil, reason = nil, comment = nil, options = {})
+        query_map                   = {}
+        query_map[:paymentMethodId] = payment_method_id unless payment_method_id.nil?
+
         @transaction_type   = 'PURCHASE'
         created_transaction = self.class.post "#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/#{account_id}/payments",
                                               to_json,
-                                              {
-                                                  :paymentMethodId => payment_method_id
-                                              },
+                                              query_map,
                                               {
                                                   :user    => user,
                                                   :reason  => reason,
@@ -35,12 +37,13 @@ module KillBillClient
       end
 
       def credit(account_id, payment_method_id = nil, user = nil, reason = nil, comment = nil, options = {})
+        query_map                   = {}
+        query_map[:paymentMethodId] = payment_method_id unless payment_method_id.nil?
+
         @transaction_type   = 'CREDIT'
         created_transaction = self.class.post "#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/#{account_id}/payments",
                                               to_json,
-                                              {
-                                                  :paymentMethodId => payment_method_id
-                                              },
+                                              query_map,
                                               {
                                                   :user    => user,
                                                   :reason  => reason,
