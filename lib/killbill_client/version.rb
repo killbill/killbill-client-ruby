36722f96db738f8f69e2f560c76ee2c887858eed
stephane brossier <sbrossier@groupon.com>
2015-03-30 16:39:52 -0700

    Bump version for release

--- lib/killbill_client/version.rb
+++ lib/killbill_client/version.rb
@@ -1,8 +1,8 @@
 module KillBillClient
   module Version
     MAJOR = 0
-    MINOR = 10
-    PATCH = 8 
+    MINOR = 11
+    PATCH = 0
     PRE = nil
 
     VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.').freeze
