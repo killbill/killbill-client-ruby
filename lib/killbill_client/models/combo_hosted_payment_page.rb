62644a3055570cb4081d1646f32db948b5862600
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-08-11 04:01:28 -0700

    payment: add support for HPP
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

---
+++ lib/killbill_client/models/combo_hosted_payment_page.rb
@@ -0,0 +1,20 @@
+module KillBillClient
+  module Model
+    class ComboHostedPaymentPage < ComboHostedPaymentPageAttributes
+
+      def build_form_descriptor(user = nil, reason = nil, comment = nil, options = {})
+        query_map = {}
+
+        self.class.post "#{HostedPaymentPage::KILLBILL_API_HPP_PREFIX}/hosted/form",
+                        to_json,
+                        query_map,
+                        {
+                            :user => user,
+                            :reason => reason,
+                            :comment => comment,
+                        }.merge(options),
+                        HostedPaymentPageFormDescriptorAttributes
+      end
+    end
+  end
+end
