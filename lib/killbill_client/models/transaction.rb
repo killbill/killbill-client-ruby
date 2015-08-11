bbfbb5fe4bfe37fe3f9690177f4f9aef5fb9a745
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-08-11 09:06:22 -0700

    transaction: add ability to authorize/purchase/credit by account external key
    See https://github.com/killbill/killbill/issues/368.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/killbill_client/models/transaction.rb
+++ lib/killbill_client/models/transaction.rb
@@ -5,51 +5,39 @@ module KillBillClient
       has_many :audit_logs, KillBillClient::Model::AuditLog
 
       def auth(account_id, payment_method_id = nil, user = nil, reason = nil, comment = nil, options = {})
-        query_map                   = {}
-        query_map[:paymentMethodId] = payment_method_id unless payment_method_id.nil?
-
-        @transaction_type   = 'AUTHORIZE'
-        created_transaction = self.class.post "#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/#{account_id}/payments",
-                                              to_json,
-                                              query_map,
-                                              {
-                                                  :user    => user,
-                                                  :reason  => reason,
-                                                  :comment => comment,
-                                              }.merge(options)
-        created_transaction.refresh(options, Payment)
+        @transaction_type = 'AUTHORIZE'
+        query_map = {}
+        create_initial_transaction("#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/#{account_id}/payments", query_map, payment_method_id, user, reason, comment, options)
       end
 
       def purchase(account_id, payment_method_id = nil, user = nil, reason = nil, comment = nil, options = {})
-        query_map                   = {}
-        query_map[:paymentMethodId] = payment_method_id unless payment_method_id.nil?
-
-        @transaction_type   = 'PURCHASE'
-        created_transaction = self.class.post "#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/#{account_id}/payments",
-                                              to_json,
-                                              query_map,
-                                              {
-                                                  :user    => user,
-                                                  :reason  => reason,
-                                                  :comment => comment,
-                                              }.merge(options)
-        created_transaction.refresh(options, Payment)
+        @transaction_type = 'PURCHASE'
+        query_map = {}
+        create_initial_transaction("#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/#{account_id}/payments", query_map, payment_method_id, user, reason, comment, options)
       end
 
       def credit(account_id, payment_method_id = nil, user = nil, reason = nil, comment = nil, options = {})
-        query_map                   = {}
-        query_map[:paymentMethodId] = payment_method_id unless payment_method_id.nil?
+        @transaction_type = 'CREDIT'
+        query_map = {}
+        create_initial_transaction("#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/#{account_id}/payments", query_map, payment_method_id, user, reason, comment, options)
+      end
 
-        @transaction_type   = 'CREDIT'
-        created_transaction = self.class.post "#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/#{account_id}/payments",
-                                              to_json,
-                                              query_map,
-                                              {
-                                                  :user    => user,
-                                                  :reason  => reason,
-                                                  :comment => comment,
-                                              }.merge(options)
-        created_transaction.refresh(options, Payment)
+      def auth_by_external_key(account_external_key, payment_method_id = nil, user = nil, reason = nil, comment = nil, options = {})
+        @transaction_type = 'AUTHORIZE'
+        query_map = {:externalKey => account_external_key}
+        create_initial_transaction("#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/payments", query_map, payment_method_id, user, reason, comment, options)
+      end
+
+      def purchase_by_external_key(account_external_key, payment_method_id = nil, user = nil, reason = nil, comment = nil, options = {})
+        @transaction_type = 'PURCHASE'
+        query_map = {:externalKey => account_external_key}
+        create_initial_transaction("#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/payments", query_map, payment_method_id, user, reason, comment, options)
+      end
+
+      def credit_by_external_key(account_external_key, payment_method_id = nil, user = nil, reason = nil, comment = nil, options = {})
+        @transaction_type = 'CREDIT'
+        query_map = {:externalKey => account_external_key}
+        create_initial_transaction("#{Account::KILLBILL_API_ACCOUNTS_PREFIX}/payments", query_map, payment_method_id, user, reason, comment, options)
       end
 
       def capture(user = nil, reason = nil, comment = nil, options = {})
@@ -106,6 +94,20 @@ module KillBillClient
         path = Payment::KILLBILL_API_PAYMENTS_PREFIX
         path += "/#{payment_id}" unless payment_id.nil?
         path
+      end
+
+      def create_initial_transaction(path, query_map, payment_method_id, user, reason, comment, options)
+        query_map[:paymentMethodId] = payment_method_id unless payment_method_id.nil?
+
+        created_transaction = self.class.post path,
+                                              to_json,
+                                              query_map,
+                                              {
+                                                  :user => user,
+                                                  :reason => reason,
+                                                  :comment => comment,
+                                              }.merge(options)
+        created_transaction.refresh(options, Payment)
       end
     end
   end
