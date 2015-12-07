4f6cb7213dcae7b72a14a29431de963802247888
Kill Bill core team <contact@killbill.io>
2015-12-07 23:14:52 +0000

    version: bump to 0.24.1
    Signed-off-by: Kill Bill core team <contact@killbill.io>

--- lib/killbill_client/version.rb
+++ lib/killbill_client/version.rb
@@ -2,7 +2,7 @@ module KillBillClient
   module Version
     MAJOR = 0
     MINOR = 24
-    PATCH = 0
+    PATCH = 1
     PRE = nil
 
     VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.').freeze
