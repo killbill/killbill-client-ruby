27dc18f2f2270e644f88fa65f111d52b2e2b0a9f
Artem Bolshakov <abolshakov@spbtv.com>
2016-03-16 14:46:19 +0300

    Take in account KillBillClient.api_key and KillBillClient.api_secret when checking for multi tenant options

--- spec/killbill_client/resource_spec.rb
+++ spec/killbill_client/resource_spec.rb
@@ -15,4 +15,61 @@ describe KillBillClient::Model::Resource do
     payment2.target_invoice_id.should == '5678'
     payment2.purchased_amount.should == 12.42
   end
+
+  describe '#require_multi_tenant_options!' do
+    let(:message) { 'nothing' }
+
+    def require_multi_tenant_options!
+      described_class.require_multi_tenant_options!(options, message)
+    end
+
+    context 'when api_key and api_secret passed as options' do
+      let(:options) do
+        {
+          api_key: 'bob',
+          api_secret: 'lazar'
+        }
+      end
+
+      it do
+        expect { require_multi_tenant_options! }.not_to raise_error
+      end
+    end
+
+    context 'when no api_key passed as options' do
+      let(:options) do
+        {
+          api_secret: 'lazar'
+        }
+      end
+
+      it do
+        expect { require_multi_tenant_options! }.to raise_error(ArgumentError, message)
+      end
+    end
+
+    context 'when no api_secret passed as options' do
+      let(:options) do
+        {
+          api_key: 'bob'
+        }
+      end
+
+      it do
+        expect { require_multi_tenant_options! }.to raise_error(ArgumentError, message)
+      end
+    end
+
+    context 'when api_key and api_secret passed as KillBillClient configuration option' do
+      let(:options) { { } }
+      before do
+        KillBillClient.stub(:api_key) { 'bob' }
+        KillBillClient.stub(:api_secret) { 'lazar' }
+      end
+
+      it do
+        expect { require_multi_tenant_options! }.not_to raise_error
+      end
+    end
+  end
 end
