a00e07fde6847e338d899a2d46d2813f33df7d34
Dali Wei <dwei@C02QV0Q4G8WM.group.on>
2016-04-07 22:52:03 -0700

    Update double encoding spec

--- spec/killbill_client/encoding_spec.rb
+++ spec/killbill_client/encoding_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe KillBillClient::API do
-  let(:expected_uri) {
-      '?controlPluginName=killbill-example-plugin&pluginProperty=contractId%3Dtest&pluginProperty=details%3D%257B%2522eventType%2522%253A%2522voidEvent%2522%252C%2522transactionType%2522%253A%2522void%2522%252C%2522contractType%2522%253A%2522temp%2522%257D'
+  let(:expected_query_params) {
+    [
+      [
+          'controlPluginName=killbill-example-plugin',
+          'pluginProperty=contractId%3Dtest',
+          'pluginProperty=details%3D%257B%2522eventType%2522%253A%2522voidEvent%2522%252C%2522transactionType%2522%253A%2522void%2522%257D'
+      ],
+      [
+          'controlPluginName=killbill-example-plugin',
+          'pluginProperty=contractId%3Dtest',
+          'pluginProperty=details%3D%257B%2522transactionType%2522%253A%2522void%2522%252C%2522eventType%2522%253A%2522voidEvent%2522%257D'
+      ]
+    ]
   }
 
   it 'should send double-encoded uri' do
@@ -13,8 +24,7 @@ describe KillBillClient::API do
     info_property.key = 'details'
     info_property.value = {
         'eventType' => 'voidEvent',
-        'transactionType' => 'void',
-        'contractType' => 'temp'
+        'transactionType' => 'void'
     }.to_json
     plugin_properties = [contract_property, info_property]
     options = {
@@ -22,8 +32,9 @@ describe KillBillClient::API do
         :pluginProperty => plugin_properties
     }
     http_adapter = DummyForHTTPAdapter.new
-    uri = http_adapter.send(:encode_params, options)
-    expect(uri).to eq(expected_uri)
+    query_string = http_adapter.send(:encode_params, options)
+    expect(query_string.chars.first).to eq('?')
+    expect(expected_query_params).to include(query_string[1..-1].split('&').sort)
   end
 end
 
