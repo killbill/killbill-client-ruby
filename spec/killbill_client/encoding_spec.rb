b11e5a21744600b23c644fd982b3917141b5c0b6
Dali Wei <dwei@C02QV0Q4G8WM.group.on>
2016-04-09 10:16:45 -0700

    Update encoding spec based on decoded properties

--- spec/killbill_client/encoding_spec.rb
+++ spec/killbill_client/encoding_spec.rb
@@ -3,16 +3,8 @@ require 'spec_helper'
 describe KillBillClient::API do
   let(:expected_query_params) {
     [
-      [
-          'controlPluginName=killbill-example-plugin',
-          'pluginProperty=contractId%3Dtest',
-          'pluginProperty=details%3D%257B%2522eventType%2522%253A%2522voidEvent%2522%252C%2522transactionType%2522%253A%2522void%2522%257D'
-      ],
-      [
-          'controlPluginName=killbill-example-plugin',
-          'pluginProperty=contractId%3Dtest',
-          'pluginProperty=details%3D%257B%2522transactionType%2522%253A%2522void%2522%252C%2522eventType%2522%253A%2522voidEvent%2522%257D'
-      ]
+        'controlPluginName=killbill-example-plugin',
+        'pluginProperty=contractId%3Dtest'
     ]
   }
 
@@ -22,10 +14,11 @@ describe KillBillClient::API do
     contract_property.value = 'test'
     info_property = KillBillClient::Model::PluginPropertyAttributes.new
     info_property.key = 'details'
-    info_property.value = {
+    info_property_hash = {
         'eventType' => 'voidEvent',
         'transactionType' => 'void'
-    }.to_json
+    }
+    info_property.value = info_property_hash.to_json
     plugin_properties = [contract_property, info_property]
     options = {
         :params => {:controlPluginName => 'killbill-example-plugin'},
@@ -34,7 +27,24 @@ describe KillBillClient::API do
     http_adapter = DummyForHTTPAdapter.new
     query_string = http_adapter.send(:encode_params, options)
     expect(query_string.chars.first).to eq('?')
-    expect(expected_query_params).to include(query_string[1..-1].split('&').sort)
+    query_params = query_string[1..-1].split('&').sort
+    expect(query_params.size).to eq(3)
+
+    # check the first two query strings
+    expect(query_params[0..1]).to eq(expected_query_params)
+
+    # decode info_property to check if it is double encoded
+    info_property_query = CGI.parse(query_params[2])
+    expect(info_property_query.size).to eq(1)
+    expect(info_property_query.keys.first).to eq('pluginProperty')
+    expect(info_property_query['pluginProperty'].size).to eq(1)
+    output_info_property = info_property_query['pluginProperty'].first.split('=')
+    expect(output_info_property.size).to eq(2)
+    expect(output_info_property[0]).to eq(info_property.key)
+    # should match if we decode it
+    expect(JSON.parse CGI.unescape(output_info_property[1])).to eq(info_property_hash)
+    # also ensure the undecoded value is different so that it was indeed encocded twice
+    expect(output_info_property[1]).not_to eq(CGI.unescape output_info_property[1])
   end
 end
 
