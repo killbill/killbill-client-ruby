46f06f4c25c9f4ec0e820f23413ec86ecc7521a4
stephane brossier <sbrossier@groupon.com>
2016-08-31 16:21:42 -0700

    Overdue: Implement ability to retrieve both per-tenant xml or json overdue config.

--- lib/killbill_client/models/overdue.rb
+++ lib/killbill_client/models/overdue.rb
@@ -1,18 +1,22 @@
 module KillBillClient
   module Model
-    class Overdue < OverdueStateAttributes
+    class Overdue < OverdueAttributes
+
+      has_many :overdue_states, KillBillClient::Model::OverdueStateConfig
 
       KILLBILL_API_OVERDUE_PREFIX = "#{KILLBILL_API_PREFIX}/overdue"
 
       class << self
-        def get_tenant_overdue_config(options = {})
+
+        def get_tenant_overdue_config(format, options = {})
 
           require_multi_tenant_options!(options, "Retrieving an overdue config is only supported in multi-tenant mode")
 
           get KILLBILL_API_OVERDUE_PREFIX,
               {},
               {
-                  :head => {'Accept' => 'application/xml'},
+                  :head => {'Accept' => "application/#{format}"},
+                  :content_type => "application/#{format}",
               }.merge(options)
         end
 
@@ -31,7 +35,7 @@ module KillBillClient
                    :reason => reason,
                    :comment => comment,
                }.merge(options)
-          get_tenant_overdue_config(options)
+          get_tenant_overdue_config('xml', options)
         end
       end
     end
