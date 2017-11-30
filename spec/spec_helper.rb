d6cf35a763f2109625b0310d6f0fc9787bbd6807
Pierre-Alexandre Meyer <pierre@mouraf.org>
2017-11-30 09:24:29 -0800

    Ruby 1.8.7 compatibility
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,7 @@ RSpec.configure do |config|
 
   config.before(:each, :integration => true) do
     # Setup a tenant for that test
-    KillBillClient.api_key = Time.now.to_i.to_s + Random.rand(100).to_s
+    KillBillClient.api_key = Time.now.to_i.to_s + rand(100).to_s
     KillBillClient.api_secret = 'S4cr3333333t!!!!!!lolz'
 
     tenant = KillBillClient::Model::Tenant.new
