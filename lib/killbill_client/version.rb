d8dfc96f4d6bf315e9265a3ba2e218d3f5ad62ba
Kill Bill core team <contact@killbill.io>
2018-01-19 13:15:25 +0000

    version: prepare release 1.12.1
    Signed-off-by: Kill Bill core team <contact@killbill.io>

--- lib/killbill_client/version.rb
+++ lib/killbill_client/version.rb
@@ -2,7 +2,7 @@ module KillBillClient
   module Version
     MAJOR = 1
     MINOR = 12
-    PATCH = 0
+    PATCH = 1
     PRE = nil
 
     VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.').freeze
